22694ac5befe313c203c710de0c1ee71367d9a05
msalinero <msalinero85@gmail.com>
2019-04-08 09:00:13 +0200

    Clustal W align

--- Source/loadSecuences.py
+++ Source/loadSecuences.py
@@ -1,5 +1,7 @@
+from Bio.Align.Applications import ClustalwCommandline
 from Bio import SeqIO
 from Bio import AlignIO
+import Bio.Align.Applications
 import tkinter as tk
 from tkinter import filedialog
 
@@ -11,24 +13,34 @@ def get_file():
     return file_path
 
 
-def get_secuences():
-    file_path = get_file()
-    return SeqIO.parse(file_path, "fasta")
+#def get_secuences():
+#    file_path = get_file()
+#    return SeqIO.parse(file_path, "fasta")
 
-def print_secuences(secs):
-    for seq_record in secs:
-        print(seq_record.id)
-        print(seq_record.seq)
-        print(len(seq_record))
+#def print_secuences(secs):
+#    for seq_record in secs:
+#        print(seq_record.id)
+#        print(seq_record.seq)
+#        print(len(seq_record))
 
 
-print("Secuencias e isoformas")
-benignSecuences = get_secuences()
-print_secuences(benignSecuences)
+#print("Secuencias e isoformas")
+#benignSecuences = get_secuences()
+#print_secuences(benignSecuences)
 
-print("Secuencias con GKD")
-GKDSecuences = get_secuences()
-print_secuences(GKDSecuences)
+#print("Secuencias con GKD")
+#GKDSecuences = get_secuences()
+#print_secuences(GKDSecuences)
 
  
+### Alineamiento con ClustalW
 
+#help(ClustalwCommandline)
+
+f = get_file()
+cline = ClustalwCommandline("clustalw2", infile=f)
+clustalw_exe = r"C:\Program Files (x86)\ClustalW2\clustalw2.exe"
+clustalw_cline = ClustalwCommandline(clustalw_exe, infile=f)
+stdout, stderr = clustalw_cline()
+# align = AlignIO.read("sec1.aln","clustal")
+# print(align)
